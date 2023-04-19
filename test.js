f52020999b8f59ef13b32df3020cdfc7247e7a4b
QuePark <gyuhapark11@gmail.com>
2023-04-19 17:53:56 +0900

    Initial Commit: getBalance

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 import { Secp256k1HdWallet } from "@cosmjs/amino";
+import { StargateClient } from "@cosmjs/stargate";
 
 (async () => {
+	const rpcEndpoint = "your rpc enpoint here";
 	const mnemonic = "your mnemonic here";
 	const wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix: "rizon" });
 	console.log(wallet);
 	const [{ address, pubkey }] = await wallet.getAccounts();
 	console.log(address);
+	const client = await StargateClient.connect(rpcEndpoint);
+	console.log("chain id: ", await client.getChainId());
+	const balance = await client.getAllBalances(address);
+	console.log("balance: ", balance);
 })();
